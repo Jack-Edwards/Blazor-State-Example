8f83703f946b2fa9b03805ec9670c1a93b53e35f
Jack-Edwards <jackedwards@protonmail.com>
2021-12-28 18:35:49 -0600

    Add project files.

---
+++ StateExample/Services/StateService.cs
@@ -0,0 +1,32 @@
+﻿namespace StateExample.Services
+{
+   public enum StateItem
+   {
+      ThingOne,
+      ThingTwo
+   }
+
+   public class StateService
+   {
+      private readonly Dictionary<StateItem, object?> State;
+
+      public StateService()
+      {
+         State = new Dictionary<StateItem, object?>();
+      }
+
+      public object? GetState(StateItem item)
+      {
+         if (State.TryGetValue(item, out var value))
+         {
+            return value;
+         }
+         return null;
+      }
+
+      public void SetState<T>(StateItem item, T value)
+      {
+         State[item] = value;
+      }
+   }
+}
